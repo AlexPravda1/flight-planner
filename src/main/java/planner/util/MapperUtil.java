44b5649fe338c425d3f9468c994c28687a209b38
AlexPravda1 <kabinkos@gmail.com>
2022-06-23 22:55:33 +0400

    fixed checkstyle

--- src/main/java/planner/util/MapperUtil.java
+++ src/main/java/planner/util/MapperUtil.java
@@ -15,15 +15,15 @@ public final class MapperUtil {
         MapperUtil.mapper = mapper;
     }
 
-    public static <FromEntity, ToEntity> List<ToEntity> mapToList(List<FromEntity> fromList,
-                                                                  final Class<ToEntity> toClass) {
+    public static <FromT, T> List<T> mapToList(List<FromT> fromList,
+                                               final Class<T> toClass) {
         return fromList
                 .stream()
                 .map(from -> mapper.map(from, toClass))
                 .collect(Collectors.toList());
     }
 
-    public static <FromEntity, ToEntity> ToEntity map(FromEntity fromObject, final Class<ToEntity> toClass) {
+    public static <FromT, T> T map(FromT fromObject, final Class<T> toClass) {
         return mapper.map(fromObject, toClass);
     }
 }
